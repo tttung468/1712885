947b323f341f5d8e05a92583bcc3ccbb61a97005
ThanhTung <tttung468@gmail.com>
2018-04-01 11:20:03 +0700

    initial project version

--- 1712885.cpp
+++ 1712885.cpp
@@ -4,20 +4,33 @@
 #include<fcntl.h> //_O_U16TEXT
 #include<io.h>    //_setmode()
 #include<malloc.h>
+#include<string.h>
 #define MaxN 2048
 
 typedef struct
 {
-	wchar_t MSSV[10];
-	wchar_t HoTen[30];
-	wchar_t Khoa[30];
-	wchar_t Email[30];
+	wchar_t MSSV[11];
+	wchar_t HoTen[31];
+	wchar_t Khoa[31];
+	wchar_t Email[31];
 	int KhoaTuyen;
-	wchar_t NgaySinh[12];
-	wchar_t HinhAnh[30];
-	wchar_t MoTa[1000];
-	wchar_t SoThich[1000];
+	wchar_t NgaySinh[13];
+	wchar_t HinhAnh[31];
+	wchar_t MoTa[1001];
+	wchar_t SoThich[1001];
 }SINHVIEN;
+
+typedef struct
+{
+	wchar_t* Title = L"<title>";
+	wchar_t* NameMSSV = L"\"Personal_FullName\">";
+	wchar_t* Khoa = L"\"Personal_Department\">";
+	wchar_t* Email = L"\"Personal_Phone\">";
+	wchar_t* ThongTin = L"Thông tin cá nhân";
+	wchar_t* SoThich = L"Sở thích";
+	wchar_t* Mota = L"Description\">";
+	wchar_t* List = L"\"TextInList\">";
+}TUKHOA;
 
 long countLines(FILE* fin)
 {
@@ -79,20 +92,13 @@ void LayThongtinSV(FILE* fin, SINHVIEN* &SV, long count)
 	}
 }
 
-int StrlenWchar(wchar_t* str)
-{
-	wchar_t* p = str;
-	while (*p++);
-	return (p - str - 1);
-}
-
 wchar_t* DatTenFile(wchar_t* MSSV)
 {
 	wchar_t str2[] = L".html";
 	wchar_t str0[] = L"WEBSITE\\";
-	int len1 = StrlenWchar(MSSV);
-	int len0 = StrlenWchar(str0);
-	int len2 = StrlenWchar(str2);
+	int len1 = wcslen(MSSV);
+	int len0 = wcslen(str0);
+	int len2 = wcslen(str2);
 	int len = len0 + len1 + len2;
 	wchar_t *FileName = (wchar_t*)calloc(len, sizeof(wchar_t));
 	if (FileName != NULL)
@@ -118,13 +124,41 @@ wchar_t* DatTenFile(wchar_t* MSSV)
 	return FileName;
 }
 
+void ChenChuoiCon(FILE* fileOut, long VtriChen, wchar_t* ChuoiChen)
+{
+
+}
+
+long TimVtriChuoiCon(wchar_t* Str, wchar_t* StrCon)
+{
+	long i = 0;
+	while (1)
+	{
+		if (Str[i] == StrCon[0])	return i;
+		i++;
+	}
+}
+
 void GhiFile(FILE* fileMauHTML, FILE* fileOut)
 {
+	TUKHOA TKhoa;
 	while (!feof(fileMauHTML))
 	{
 		wchar_t str[MaxN];
+		long DauDong = ftell(fileOut);					//vị trí con trỏ fileOut ở đầu dòng
+		long VtriChen;
 		fgetws(str, MaxN, fileMauHTML);
 		fputws(str, fileOut);
+		long CuoiDong = ftell(fileOut);					//vị trí con trỏ fileOut ở cuối dòng
+		if (wcsstr(str, TKhoa.Title))
+		{
+			VtriChen = DauDong + TimVtriChuoiCon(str, TKhoa.Title);
+			wprintf(L"0x%d\n", DauDong);
+			wprintf(L"0x%d\n", CuoiDong);
+			wprintf(L"0x%d\n", VtriChen);
+			wprintf(L"0x%d\n", str);
+			wprintf(L"0x%d\n", wcsstr(str, TKhoa.Title));
+		}
 	}
 }
 
